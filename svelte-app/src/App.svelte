263f8a7a1fd19949052ef2bdfd282cf16ed45f91
Jeremyw <jeremyw@4zonelogistics.com>
2024-01-27 12:20:24 +0000

    Initial Commit-- -Added router to App.svelte -Added Navbar -Added Footer

--- svelte-app/src/App.svelte
+++ svelte-app/src/App.svelte
@@ -1,21 +1,25 @@
 <script>
 	export let name;
 	import { Router, Route } from 'svelte-routing';
+	import Navbar from './components/Navbar.svelte';
+	import Footer from './components/Footer.svelte';
 	import Home from './routes/Home.svelte';
 	// Import other pages and components
 </script>
 
-<Router>
-	<Route path="/" component={Home} />
-</Router>
-
-
 <main>
+	<Navbar />
+	<Router>
+		<Route path="/" component="{Home}"/>
+		/* Define other paths here */
+	</Router>
 	<h1>Hello {name}!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<Footer />
 </main>
 
 <style>
+	/* Global and specific styles */
 	main {
 		text-align: center;
 		padding: 1em;
